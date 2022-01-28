da7ce62021b4c53ac87ff3a103a2b1d63708528e
Irman23 <73824877+Irman23@users.noreply.github.com>
2022-01-28 23:30:43 +0100

    ispravljen.

--- zadatak19.cpp
+++ zadatak19.cpp
@@ -1,111 +1,129 @@
 #include<iostream>
 using namespace std;
 
+//Napišite program u kojem ćete kreirati niz od 5 studenata.
+//Kreirajte funkcije :
+//· za unos obilježja studenta – neka funkcija prima pokazivač na student;
+//· za ispis obilježja studenta – neka funkcija prima pokazivač na student;
+//· za validacija unesenog datuma rođenja – funkcija prima pokazivač na datum a vraća true ako
+//je datum validan(npr. 2.2.2002.) ili vraća false ako datum nije validan(npr. 17.23.100000.);
+//· za prebrojavanje studenata čiji datum rođenja nije validan – funkcija prima pokazivač na
+//student;
+//· pronalaženje prvog studenta(u nizu) čiji datum rođenja nije validan; funkcija prima i vraća
+//pokazivač na student;
+//· pozvati sve prethodne funkcije preko pokazivača;
+
 struct datum
 {
 	int d, m, g;
-	bool tacan(int d, int m, int g)
-	{
-		if (g > 2002 || g < 1960)
-			return false;
-		if (g % 4 == 0 && m == 2)
-			if (d > 29 || d < 1)
-				return false;
-		if (g % 4 != 0 && m == 2)
-			if (d > 28 || d < 1)
-				return false;
-		if (m < 1 || m>12)
-			return false;
-		if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
-		{
-			if (d > 31 || d < 1)
-				return false;
-		}
-		else
-			if (d > 30 || d < 1)
-				return false;
-
-		return true;
-		
-
-	}
 	void unosD()
 	{
-		cout << "Unesite dan, mjesec i godinu: ";
 		cin >> d >> m >> g;
-		/*while (!tacan(d, m, g))
-		{
-			cout << "Unesite dan, mjesec i godinu: ";
-			cin >> d >> m >> g;
-		}
-		*/
+	}
+	void ispisD()
+	{
+		if (d < 10)
+			cout << 0 << ".";
+		else
+			cout << d << ".";
+		if (m < 10)
+			cout << 0 << m << ".";
+		else
+			cout << m << ".";
+		cout << g << ".";
 	}
 };
-struct student
+//--------------------------------------------------
+bool validacijaD(datum* date)
+{
+	if (date->g < 1940 || date->g>2002)
+		return false;
+	if (date->g % 4 == 0 && date->m == 2)
+		if (date->d < 1 || date->d>29)
+			return false;
+	if (date->g % 4 != 0 && date->m == 2)
+		if (date->d < 1 || date->d>28)
+			return false;
+	if (date->m < 1 || date->m>12)
+		return false;
+	if (date->m == 4 || date->m == 6 || date->m == 9 || date->m == 11)
+		if (date->d < 1 || date->d>30)
+			return false;
+	if (date->m == 1 || date->m == 3 || date->m == 5 || date->m == 7 || date->m == 8 || date->m == 10 || date->m == 12)
+		if (date->d < 1 || date->d>31)
+			return false;
+	return true;
+}
+//--------------------------------------------------
+struct Student
 {
 	int IB;
 	char ImeiPrezime[30];
 	datum rodjenja;
-
-	void ISPIS()
-	{
-		cout << "Redni broj: " << IB << endl;
-		cout << "Ime i prezime: " << ImeiPrezime << endl;
-		cout << "Datum rodjenja: " << rodjenja.d<<"."<<rodjenja.m <<"."<<rodjenja.g<< endl;
-	}
+	datum* datumptr = &rodjenja;
 };
-void UNOS(student* studenti)
+//--------------------------------------------------
+void unosS(Student* student)
 {
-	cout << "Unesite IB: "; cin >> studenti->IB;
+	cout << "Unesite IB studenta: ";
+	cin >> student->IB;
 	cin.ignore();
-	cout << "Unesite ime i prezime: "; cin.getline(studenti->ImeiPrezime,30);
-	cout << "Unesite datum rodjenja"; studenti->rodjenja.unosD();
-
+	cout << "Unesite ime i prezime studenta: ";
+	cin.getline(student->ImeiPrezime, 30);
+	cout << "Unesite datum rodjenja studenta: ";
+	student->rodjenja.unosD();
+}
+void ispisS(Student* student)
+{
+	cout << "IB: " << student->IB << endl;
+	cout << "Ime i prezime: " << student->ImeiPrezime << endl;
+	cout << "Datum rodjenja: ";
+	student->rodjenja.ispisD();
+	cout << endl;
 
 }
-
-int provjera(student* studenti, int size)
+//--------------------------------------------------
+int prebrojavanje(Student* student, int vel)
 {
 	int brojac = 0;
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < vel; i++)
 	{
-		if (studenti->rodjenja.tacan(studenti->rodjenja.d, studenti->rodjenja.m, studenti->rodjenja.g) == false)
+		if (!validacijaD((student + i)->datumptr))
 			brojac++;
-		studenti++;
 	}
 	return brojac;
 }
-student* prvi(student* studenti, int size)
+//---------------------------------------------------
+Student* pronalazenje(Student* student, int vel)
 {
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < vel; i++)
 	{
-		if ((studenti+i)->rodjenja.tacan((studenti+i)->rodjenja.d, (studenti+i)->rodjenja.m, (studenti+i)->rodjenja.g) == false)
-			return (studenti + i);
+		if (!validacijaD((student + i)->datumptr))
+			return student + i;
 	}
+	return nullptr;
 }
 void main()
 {
-	int size = 5;
-	student* studenti = new student[size];
+	Student niz[5];
 	for (int i = 0; i < 5; i++)
 	{
-		UNOS(studenti+i);
+		unosS(niz + i);
 	}
+	cout << endl;
 	for (int i = 0; i < 5; i++)
 	{
-		(studenti+i)->ISPIS();
+		ispisS(niz + i);
 	}
-	
-	cout << "Pogresan unos datuma: " << (provjera(studenti,size))<< " ucenik/a" << endl;
-
-	cout << "Prvi sa pogresnim unosom datuma je: " << endl;
-	prvi(studenti, size)->ISPIS();
-
-	
-
-
-	delete[]studenti;
-	studenti = nullptr;
-
+	cout << endl;
+	//----------------------------------------------------------------------------------------------
+	if (pronalazenje(niz, 5) == nullptr)
+		cout << "Svi studenti imaju validan datum rodjenja." << endl;
+	else
+	{
+		cout << "Broj studenata sa nevalidnim datumom rodjenja: " << prebrojavanje(niz, 5) << endl;
+		cout << endl;
+		cout << "Prvi student sa nevalidnim datumom rodjenja: " << pronalazenje(niz, 5)->ImeiPrezime << endl;
+	}
 
 }
